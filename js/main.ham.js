7af2663d0f6d6a3c6e75032f6847b3aeb11afd16
da2ilva <abdullokh.narimonov@gmail.com>
2017-06-29 20:49:16 +0300

    home page ratings blocks javascript active

--- js/main.ham.js
+++ js/main.ham.js
@@ -1,42 +1,60 @@
 (function($) {
-    $(document).ready(function() {
+        $(document).ready(function() {
 
-        $('.hamburger').on('click', function() {
-            $('.hamburger').toggleClass('is-active');
-            $('.categories').slideToggle(500);
-        });
-        $('.latest-news__content').slick({
-            dots: false,
-            infinite: true,
-            speed: 400,
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            autoplay: false,
-            autoplaySpeed: 4000,
-
-
-        });
-        $('.spec-opinion__content').slick({
-            dots: false,
-            infinite: true,
-            speed: 400,
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            autoplay: false,
-            autoplaySpeed: 4000,
+                $('.hamburger').on('click', function() {
+                    $('.hamburger').toggleClass('is-active');
+                    $('.categories').slideToggle(500);
+                });
+                $('.latest-news__content').slick({
+                    dots: false,
+                    infinite: true,
+                    speed: 400,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    autoplay: false,
+                    autoplaySpeed: 4000,
 
 
-        });
-        $('.our-partners__content').slick({
-            dots: false,
-            infinite: true,
-            speed: 400,
-            slidesToShow: 5,
-            slidesToScroll: 1,
-            autoplay: false,
-            autoplaySpeed: 4000,
+                });
+                $('.spec-opinion__content').slick({
+                    dots: false,
+                    infinite: true,
+                    speed: 400,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    autoplay: false,
+                    autoplaySpeed: 4000,
 
 
-        });
-    });
-}(jQuery));
+                });
+                $('.our-partners__content').slick({
+                    dots: false,
+                    infinite: true,
+                    speed: 400,
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    autoplay: false,
+                    autoplaySpeed: 4000,
+
+
+                });
+
+                //rating tabs
+                $("#rating__type_id-1").addClass("active");
+                $(".user-ratings:not(.rating__type_id-1)").hide();
+
+
+                $(".rating__type").click(function() {
+                        var rating__type_id = $(this).attr("id");
+                        // alert(rating__type_id);
+                        // alert("hi" + "friends");
+                        $(".user-ratings").hide();
+                        $("." + rating__type_id).show();
+                        $(".rating__type").removeClass('active');
+                        $(this).addClass("active");
+
+
+                        });
+
+                });
+        }(jQuery));
