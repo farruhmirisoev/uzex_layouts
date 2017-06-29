601577389dc02421f44fd3643a4ee088bab288ee
da2ilva <abdullokh.narimonov@gmail.com>
2017-06-29 19:28:29 +0300

    home page blocks style

--- js/main.ham.js
+++ js/main.ham.js
@@ -5,14 +5,36 @@
             $('.hamburger').toggleClass('is-active');
             $('.categories').slideToggle(500);
         });
-        $('.slider').slick({
-            dots: true,
-            infinite: false,
-            speed: 300,
-            slidesToShow: 1,
+        $('.latest-news__content').slick({
+            dots: false,
+            infinite: true,
+            speed: 400,
+            slidesToShow: 2,
             slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
+            autoplay: false,
+            autoplaySpeed: 4000,
+
+
+        });
+        $('.spec-opinion__content').slick({
+            dots: false,
+            infinite: true,
+            speed: 400,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            autoplay: false,
+            autoplaySpeed: 4000,
+
+
+        });
+        $('.our-partners__content').slick({
+            dots: false,
+            infinite: true,
+            speed: 400,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            autoplay: false,
+            autoplaySpeed: 4000,
 
 
         });
